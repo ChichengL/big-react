7926c6f0ed02731997e6d12f9edcef961074b30b
chenshuangbin <chenshuangbin@bytedance.com>
2025-01-26 15:28:11 +0800

    feat: jsx 完成及其打包

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,63 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ReactElementType,
+	ElementType,
+} from 'shared/ReactTypes';
+// ReactElemnt
+const ReactElemnt = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props,
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		key,
+		ref,
+		props,
+		__mark: 'chichengl',
+		type,
+	};
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...children: any[]) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			// 如果是自己的prop，则添加到props中
+			props[prop] = val;
+		}
+	}
+
+	const childrenLength = children.length;
+	if (childrenLength === 1) {
+		props.children = children[0];
+	} else if (childrenLength > 1) {
+		props.children = children;
+	}
+
+	return ReactElemnt(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
---
+++ script/rollup/utils.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url'; // 导入 fileURLToPath
+import ts from 'rollup-plugin-typescript2';
+import cjs from '@rollup/plugin-commonjs';
+
+const __filename = fileURLToPath(import.meta.url); // 将 URL 转换为文件路径
+const __dirname = path.dirname(__filename); // 获取目录路径
+const pkgPath = path.resolve(__dirname, '../../packages');
+const distPath = path.resolve(__dirname, '../../dist/node_modules');
+
+export function resolvePkgPath(pkgName, isDist) {
+	if (isDist) {
+		return `${distPath}/${pkgName}`;
+	}
+	return `${pkgPath}/${pkgName}`;
+}
+export function getPackageJson(pkgName) {
+	// 包路径
+	const path = `${resolvePkgPath(pkgName)}/package.json`;
+	const str = fs.readFileSync(path, { encoding: 'utf-8' });
+	return JSON.parse(str);
+}
+
+export function getBaseRollupPlugins({ typescript = {} } = {}) {
+	return [cjs(), ts(typescript)];
+}
