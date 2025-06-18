a5627ec13f42a0e41adca4a8c26cc239385f1584
chenshuangbin <chenshuangbin@bytedance.com>
2025-06-18 20:20:13 +0800

    feat: 添加vite+demos实时调试

---
+++ demos/test-fc/main.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+function App() {
+	return <div>hello</div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
