4e703e12bf59143274a271e823e91372b2b00dd6
chenshuangbin <chenshuangbin@bytedance.com>
2025-06-25 21:51:57 +0800

    feat: 暂存

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,4 +1,20 @@
 import { jsxDEV } from './src/jsx';
+import currentDispatcher, {
+	Dispatcher,
+	resolveDispatcher,
+} from './src/currentDispatcher';
+export const useState: Dispatcher['useState'] = (initialState: any) => {
+	const dispatcher = resolveDispatcher();
+	if (!dispatcher) {
+		throw new Error('hooks只能在函数组件中调用');
+	}
+	return dispatcher.useState(initialState);
+};
+
+//实现内部数据共享层
+export const __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED = {
+	currentDispatcher,
+};
 export default {
 	version: '0.0.0',
 	createElement: jsxDEV,
