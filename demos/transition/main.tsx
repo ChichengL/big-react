c3bef2f93935c81d427b98eaff41c66e0f6ed58f
chichengl <2560720865@qq.com>
2025-09-08 17:01:15 +0800

    feat: 实现useTransition的例子

---
+++ demos/transition/main.tsx
@@ -0,0 +1,67 @@
+import ReactDOM from 'react-dom/client';
+// import { useState, useTransition, startTransition } from 'react';
+import { useState } from 'react';
+function App() {
+	const [tab, setTab] = useState('about');
+
+	return (
+		<>
+			<div style={{ display: 'flex', gap: '8px' }}>
+				<TabButton isActive={tab === 'about'} onClick={() => setTab('about')}>
+					Index
+				</TabButton>
+				<TabButton isActive={tab === 'post'} onClick={() => setTab('post')}>
+					Post
+				</TabButton>
+				<TabButton
+					isActive={tab === 'contact'}
+					onClick={() => setTab('contact')}
+				>
+					Contact
+				</TabButton>
+			</div>
+			<div>
+				{tab === 'about' && <About />}
+				{tab === 'post' && <Posts />}
+				{tab === 'contact' && <Contact />}
+			</div>
+		</>
+	);
+}
+function TabButton(props) {
+	const { isActive, onClick } = props;
+	return isActive ? (
+		<strong>{props.children}</strong>
+	) : (
+		<button onClick={onClick}>{props.children}</button>
+	);
+}
+function About() {
+	return <div>About Myself</div>;
+}
+function Posts() {
+	const [num] = useState(100);
+	return (
+		<ul>
+			{new Array(num).fill(0).map((_, i) => {
+				return <Item key={i}>{i}</Item>;
+			})}
+		</ul>
+	);
+}
+function Item({ children }) {
+	const now = performance.now();
+	while (performance.now() - now < 4) {
+		// 阻塞渲染
+	}
+	return <li>{children}</li>;
+}
+function Contact() {
+	return (
+		<ul>
+			<li>联系我：xxx</li>
+		</ul>
+	);
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
